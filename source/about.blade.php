54e240cf05315921a07c8707a90127bb5ec419d2
Knut Leborg <knut@lepa.no>
2022-04-13 23:13:17 +0200

    wip

--- source/about.blade.php
+++ source/about.blade.php
@@ -15,7 +15,7 @@ description: A little bit about the site
 
     <p class="mb-6">Currently focusing on eCommerce, electronic invoicing, etc.</p>
 
-    <p class="mb-6">Using TALL stack or just plain Laravel wherever practical, but has been known to dabble in PowerShell, C#, Python, Java, and others.</p>
+    <p class="mb-6">Using TALL stack or just plain Laravel wherever practical, but has been known to dabble in PowerShell, C#, Python, Java, and others. Somehow ended up having to run Laravel apps in a variety of Windows/Microsoft environments, which isn't always a walk in the park.</p>
 
     <p class="mb-6">
         <a href="https://github.com/knutle">GitHub</a> | <a href="https://selby.no">Selby AS (Employer)</a>
